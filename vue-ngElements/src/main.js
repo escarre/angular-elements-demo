dcc8a3413eef7010a798def991eea0e9fade7000
Elizabeth Carretto <elizabeth.carretto@bullhorn.com>
2018-05-23 17:34:52 -0500

    Adding a react app

--- vue-ngElements/src/main.js
+++ vue-ngElements/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+// basic vue app
+// created with the vue cli
+//just need to add our selector to the ignored elements
 Vue.config.productionTip = false;
-Vue.config.ignoredElements = ['app-button'];
+Vue.config.ignoredElements = ['ng-basic'];
 new Vue({
   render: h => h(App),
 }).$mount('#app');
